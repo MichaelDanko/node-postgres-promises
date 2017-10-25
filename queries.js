b0dc64ebe85a028d5720d4930d50ac59a471af63
Michael Danko <Michael.Danko@gnvtech.com>
2017-10-25 17:54:37 -0500

    added connection string

--- queries.js
+++ queries.js
@@ -6,11 +6,23 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/puppies';
+var connectionString = ("postgres://giarkjrnadplmu:c648a406d61eecd51dcd136193d39fe32471a0f3b5dea902f33828f28d2a81c7@ec2-54-235-254-251.compute-1.amazonaws.com:5432/d26ol6spedfe3");
+
+//var connectionString = ( process.env.DATABASE_URL || "postgres://giarkjrnadplmu:c648a406d61eecd51dcd136193d39fe32471a0f3b5dea902f33828f28d2a81c7@ec2-54-235-254-251.compute-1.amazonaws.com:5432/d26ol6spedfe3");
+console.log(connectionString);
 var db = pgp(connectionString);
 
 function getAllPuppies(req, res, next) {
-  db.any('select * from pups')
+  db.connect()
+      .then(obj => {
+                obj.done(); // success, release the connection;
+            })
+      .catch(error => {
+                console.log('ERROR:', error.message || error);
+            });
+
+  console.log(db)
+  db.any('select * from question_answer')
     .then(function (data) {
       res.status(200)
         .json({
@@ -18,7 +30,7 @@ function getAllPuppies(req, res, next) {
           data: data,
           message: 'Retrieved ALL puppies'
         });
-    })
+   })
     .catch(function (err) {
       return next(err);
     });
