a9082c332c45d7989aafeed32bb610964106d0fb
andrewgremlich <andrew.gremlich@gmail.com>
2017-10-26 09:16:22 -0500

    updated /api/insertQuestion api endpoint

--- apis/posts.js
+++ apis/posts.js
@@ -1,7 +1,16 @@
+const insertQuestion = require('./db/insertQuestion.js')
+
 function posts(server, client) {
 
   server.post('/api/insertQuestion', (req, res, next) => {
     console.log(req.body)
+
+    let question = req.body.question,
+      entity = req.body.entity,
+      userId = req.body.userId
+
+    insertQuestion(client, question, entity, userId)
+
     return next()
   })
 }
