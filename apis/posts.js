948bb6fbc2abe7b88f033b5fa469dbdb3d9f3a2f
andrewgremlich <andrew.gremlich@gmail.com>
2017-10-26 11:58:03 -0500

    added /api/answerQuestion/

--- apis/posts.js
+++ apis/posts.js
@@ -4,6 +4,24 @@ function posts(server, client) {
 
   //do submit upvote api
 
+  server.post('/api/answerQuestion', (req, res, next) =>{
+    let questionId = req.body.questionId,
+      answerId = req.body.answerId,
+      answer = req.body.answer
+
+      client.query(`
+          INSERT INTO ANSWERS (ANSWER_ID, ANSWER)
+          VALUES ('${answerId}', '${answer}');
+
+          INSERT INTO QUESTIONS_TO_ANSWERS (QUEST_ID, ANSWER_ID)
+          VALUES ('${questionId}', '${answerId}');
+        `, (err, resp) => {
+        if (err) throw err
+        console.log(resp.rows)
+        res.send(resp.rows)
+      })
+  })
+
   server.post('/api/insertQuestion', (req, res, next) => {
     let question = req.body.question,
       entity = req.body.entity,
